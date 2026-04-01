16ffd75b200790371d318fb4ea4c4b7880e3b349
AkMebo <117305774+AkMebo@users.noreply.github.com>
2026-04-01 13:34:29 +0300

    Load healthcare management dataset using kagglehub
    Import kagglehub and load healthcare dataset into DataFrames.

--- dataanalysis_health.py
+++ dataanalysis_health.py
@@ -1,1 +1,28 @@
+import kagglehub; print('kagglehub imported successfully') # download dependencies pip install kagglehub[pandas-datasets]
+from kagglehub import KaggleDatasetAdapter
 
+# load dataset directly into Dataframe
+dataset = "anouskaabhisikta/healthcare-management-system"
+files = [
+    "Appointment.csv",
+    "Billing.csv",
+    "Doctor.csv",
+    "Medical Procedure.csv",
+    "Patient.csv",
+]
+
+dfs = {}
+for file_name in files:
+    dfs[file_name] = kagglehub.dataset_load(
+        KaggleDatasetAdapter.PANDAS, #Give me this data as a pandas DataFrame
+        dataset, #Go to this specific dataset on Kaggle
+        file_name, ## Load a DataFrame with a specific version of a CSV
+    )
+    print(file_name, "loaded:", dfs[file_name].shape)
+
+# usage:
+appointments = dfs["Appointment.csv"]
+billing = dfs["Billing.csv"]
+doctor = dfs["Doctor.csv"]
+procedures = dfs["Medical Procedure.csv"]
+patients = dfs["Patient.csv"]
